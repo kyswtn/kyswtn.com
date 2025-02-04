7eebab6a7868f4a76281b360f247a781b531b942
Kyaw <mail@kyswtn.com>
2025-02-05 00:06:00 +0700

    feat: add first commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            bun
+            biome
+          ];
+        };
+    };
+}
