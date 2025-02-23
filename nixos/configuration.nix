c2fd9eefdbe51fe7f64f926e0493a5f44478da52
Kyaw <mail@kyswtn.com>
2025-02-24 06:39:08 +0700

    feat: update nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,11 +8,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Mandatory configurations.
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  time.timeZone = "Europe/Helsinki";
+  time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  # Nix settings.
   nix = {
     settings = {
       auto-optimise-store = true;
@@ -26,17 +28,12 @@
     };
   };
 
-  virtualisation.docker.enable = true;
   users.users.root = {
     extraGroups = [ "docker" ];
     openssh.authorizedKeys.keys = [ (builtins.readFile ../id_ed25519.pub) ];
   };
 
-  environment.systemPackages = map lib.lowPrio [
-    pkgs.curl
-    pkgs.gitMinimal
-  ];
-
+  # Networking.
   services.openssh = {
     enable = true;
     settings = {
@@ -49,6 +46,17 @@
   services.tailscale.enable = true;
   networking.firewall.allowedTCPPorts = [ 80 443 22 ];
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+
+  # Docker.
+  virtualisation.docker.enable = true;
+  system.activationScripts.mkDockerNetworks = let docker = "${pkgs.docker}/bin/docker"; in ''
+    ${docker} network inspect traefik >/dev/null 2>&1 || ${docker} network create traefik
+  '';
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
 
   system.stateVersion = "24.11";
 }
